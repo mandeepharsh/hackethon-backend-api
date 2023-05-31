74c9c639a170ef8bf7f6962cf70d5792c83145ad
mandeepharsh <harshman47@gmail.com>
2023-05-31 01:16:14 -0400

    add json files

---
+++ routes/peers.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const fs =  require('fs');
+
+const filepath  = "./data/peers.json";
+
+router.get("/peers", (req,res) =>{
+    const data = fs.readFileSync(filepath)
+    res.json(JSON.parse(data))
+})
+
+router.post("/peers",(req,res))
+
+module.exports = router;
